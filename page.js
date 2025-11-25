4922d747c9c3dfccfb394d273a653ddd96001c5b
DesainProyek-20 <desainproyek20@gmail.com>
2025-11-25 13:41:07 +0700

    Create page.js

---
+++ page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h1>EcoGlo Dashboard</h1>
+      <p>Dashboard berhasil deploy 🎉</p>
+    </main>
+  );
+}
